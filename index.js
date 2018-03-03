517924c44679bc546c8f7df7c65063463fef98ab
Tare Gaskin <tare.dgg@gmail.com>
2018-03-03 17:10:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos);
 
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>')}
   </ul>`;
-  
+
   document.getElementById("repositories").innerHTML = repoList;
 }
 
